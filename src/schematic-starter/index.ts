2cf03205dc57209b1629911b126c2140f24aeef4
root <root@STORM-PC.localdomain>
2019-06-07 00:27:24 -0400

    move

--- src/schematic-starter/index.ts
+++ src/schematic-starter/index.ts
@@ -10,11 +10,14 @@ import {
 import { strings } from "@angular-devkit/core";
 
 // declare AddFilesInterface
+interface AddFilesInterface {
+  name: string;
+}
 
-export function schematicStarter(_options: any): Rule {
+export function schematicStarter(_options: AddFilesInterface): Rule {
   return (tree: Tree, context: SchematicContext) => {
     // todo: use the template() function and provide the strings and _options members
-    const rules: Rule[] = [];
+    const rules: Rule[] = [template({ ...strings, ..._options })];
 
     const source = url("./files");
     return apply(source, rules)(context);
